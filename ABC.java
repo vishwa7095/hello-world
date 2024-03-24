f5a8562f8b6dcfc240d7e73a7dce1a7d9460a088
vishwa7095 <vishwanathkalathuru@gmail.com>
2024-03-24 16:51:36 +0530

    first commit

---
+++ ABC.java
@@ -0,0 +1,20 @@
+package package3;
+import package1.*;
+import package2.*;
+class ABC
+{
+	public static void main(String args[])
+	{
+	
+		package1.X x=new package1.X();
+		x.show();
+		package1.y y=new package1.Y();
+		y.show();
+		package2.X x1=new package2.X();
+		x1.show();
+		package2.Z z=new package2.Z();
+		z.show();
+
+	}
+}
+	
